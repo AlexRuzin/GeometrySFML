c2ad5ba61040e160f0351136d33af783ef79c7d7
Alex Ruzin <stan.ruzin@gmail.com>
2023-11-20 23:57:13 -0500

    Adding logic to sfml wrapper

--- GeometrySFMLCore/Project1/sfml.cpp
+++ GeometrySFMLCore/Project1/sfml.cpp
@@ -4,6 +4,7 @@
 
 #include <string>
 #include <thread>
+#include <mutex>
 
 SfmlError SfmlCoreWindow::StartWindowThread(void)
 {
@@ -43,6 +44,7 @@ void SFML_THREAD SfmlCoreWindow::windowThread(void)
 
     renderWindow = new sf::RenderWindow(sf::VideoMode(winWidth, winHeight), winName);
 
+#if 0
     sf::CircleShape circle(100);
     circle.setFillColor(sf::Color::Green);
     circle.setPosition(350, 250); // position the circle in the center
@@ -59,8 +61,11 @@ void SFML_THREAD SfmlCoreWindow::windowThread(void)
     circleCenter.y += radius;
     radiusLine.setPosition(circleCenter);
     radiusLine.setOrigin(0, 1); // Set origin to one of the ends of the line
+#endif
 
     while (renderWindow->isOpen() && !windowThreadSync.IsSignaled()) {
+        std::unique_lock<std::mutex> mlock(drawObjectSync);
+
         // Event processing
         sf::Event event;
         while (renderWindow->pollEvent(event))
@@ -71,8 +76,34 @@ void SFML_THREAD SfmlCoreWindow::windowThread(void)
 
         // Rendering
         renderWindow->clear(sf::Color::Black);
+
+        for (std::list<SFML_OBJECT>::iterator i = drawObjectInput.begin(); i != drawObjectInput.end(); i++) {
+            if (!i->isDrawn) {
+                
+                i->isDrawn = true;
+
+                switch(i->type) {
+                case SFML_OBJ_CIRCLE:
+                    i->circle = new sf::CircleShape(i->radius);
+                    i->circle->setFillColor(i->color);
+                    i->circle->setPosition(i->posX, i->posY);
+
+                    break;
+                case SFML_OBJ_LINE:
+
+
+                    break;
+                default:
+                    break;
+                }               
+            }
+        }
+
+#if 0
         renderWindow->draw(circle);
         renderWindow->draw(radiusLine);
+#endif
+
         renderWindow->display();
     }
 
@@ -81,4 +112,60 @@ void SFML_THREAD SfmlCoreWindow::windowThread(void)
     renderWindow = nullptr;
 
     return;
+}
+
+SfmlError SfmlCoreWindow::DrawCircle(uint32_t x, uint32_t y, uint32_t radius, unsigned long color, SFML_OBJECT **objOut)
+{
+    std::unique_lock<std::mutex> mlock(drawObjectSync);
+
+    if (!renderWindow) {
+        return SFML_ERROR_NOT_RUNNING;
+    }
+
+    SFML_OBJECT obj;
+    obj.isDrawn = false;
+    obj.type = SFML_OBJ_CIRCLE;
+    obj.posX = x;
+    obj.posY = y;
+    obj.radius = radius;
+    obj.color = convertHexToSfmlColor(color);
+
+    drawObjectInput.push_back(obj);
+
+    if (objOut) {
+        *objOut = &drawObjectInput.back();
+    }
+
+    return SFML_ERROR_OK;
+}
+
+SfmlError SfmlCoreWindow::DeleteDrawnObject(const SFML_OBJECT* obj)
+{
+    std::unique_lock<std::mutex> mlock(drawObjectSync);
+
+    if (!obj) {
+        return SFML_ERROR_INPUT;
+    }
+
+    
+
+    return SFML_ERROR_OK;
+}
+
+sf::Color SfmlCoreWindow::convertHexToSfmlColor(unsigned long color)
+{
+    sf::Color out;
+
+    // default is opaque
+    out.a = 0xff;
+
+    out.r = (color >> 16) & 0xff;
+    out.g = (color >> 8) & 0xff;
+    out.b = color & 0xff;
+
+    if (color > 0x00ffffff) {
+        out.a = (color >> 24) & 0xff;
+    } 
+
+    return out;
 }
--- GeometrySFMLCore/Project1/sfml.h
+++ GeometrySFMLCore/Project1/sfml.h
@@ -6,17 +6,49 @@
 
 #include <string>
 #include <thread>
+#include <memory>
+#include <list>
+#include <mutex>
 
 #define SFML_THREAD                             __cdecl
 
-enum SfmlError {
+typedef enum SfmlError {
     SFML_ERROR_OK,
+    SFML_ERROR_INPUT,
     SFML_ERROR_SFML_INIT,
     SFML_ERROR_THREAD_ALREADY_RUNNING,
     SFML_ERROR_NOT_RUNNING
-};
+} SFML_ERROR;
+
+typedef enum sfmlDrawObjectType {
+    SFML_OBJ_CIRCLE,
+    SFML_OBJ_LINE
+} SFML_DRAW_OBJECT_TYPE;
+
+#define SFML_OBJECT_IDENTIFIER                  std::string;
 
 class SfmlCoreWindow {
+private:
+    typedef struct sfmlObjectSpecs {
+        bool                                    isDrawn;
+
+        SFML_DRAW_OBJECT_TYPE                   type;
+
+        uint32_t                                posX;
+        uint32_t                                posY;
+
+        uint32_t                                length;
+        uint32_t                                height;
+
+        uint32_t                                radius;
+
+        sf::Color                               color;
+
+        sf::CircleShape                         *circle;
+        sf::RectangleShape                      *rectangle;
+    } SFML_OBJECT, *PSFML_OBJECT;
+
+
 private:
     const uint32_t                              winWidth;
     const uint32_t                              winHeight;
@@ -26,6 +58,9 @@ private:
     std::thread                                 *windowThreadObj;
 
     sf::RenderWindow                            *renderWindow;
+
+    std::mutex                                  drawObjectSync;
+    std::list<SFML_OBJECT>                      drawObjectInput;
 
 public:
     SfmlCoreWindow(uint32_t winWidth, uint32_t winHeight, std::string winName) :
@@ -44,10 +79,20 @@ public:
 
     }
 
+    // Initialize SFML form
     SfmlError StartWindowThread(void);
     SfmlError StopWindowThread(void);
 
+    // Color is in hexadecimal format (and alpha)
+    SfmlError DrawCircle(uint32_t x, uint32_t y, uint32_t radius, unsigned long color, SFML_OBJECT **objOut);
+
+    // Delete an object from the draw queue
+    SfmlError DeleteDrawnObject(const SFML_OBJECT *obj);
+
 private:
     void SFML_THREAD windowThread(void);
+
+private:
+    static sf::Color convertHexToSfmlColor(unsigned long color);
 };
 
